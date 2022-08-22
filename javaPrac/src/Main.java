37bec050e818f971f5f6a53643901c368e6db14b
Rohan Bhandari <bhandarirohan556@gmail.com>
2022-08-22 09:02:44 -0400

    updated

--- javaPrac/src/Main.java
+++ javaPrac/src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String [] args){
+        // primitive data structures
+//        int x = 15;
+//        short y = 12;
+//        byte s = 12;
+//        long z = 12132123L;
+//        char c = 'a';
+
+        String [] arr = new String[50];
+        int [] arr2 = new int[]{1,2,3,4,5,6,7,1};
+
+        Set<Integer> test = new HashSet<>();
+        for(int i = 0 ; i<6; i++){
+            test.add(i);
+        }
+        test.add(5);
+
+//        System.out.println(arr);
+//        Arrays.stream(arr2).forEach(System.out::println)
+        System.out.println(test);
+    }
+
 }
---
+++ smallProjects/prac.py
@@ -0,0 +1,9 @@
+
+tuple = (1,2.3,'a','a',4,4,4)
+bl
+print(tuple.count(4))
+set = {'a','a','a','1',2}
+test = set
+test.remove('a')
+string  = 'mynameisrohan'
+print(test)
